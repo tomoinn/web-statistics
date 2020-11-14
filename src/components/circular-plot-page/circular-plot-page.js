f4220e6c34505d87e1a8c18bb8b7d5236a28a751
Thomas Down <thomas@tad.me.uk>
2020-11-14 11:38:42 +0000

    Remove AMD-style import/export.

--- src/components/circular-plot-page/circular-plot-page.js
+++ src/components/circular-plot-page/circular-plot-page.js
@@ -1,78 +1,78 @@
-define(["knockout", "./circular-plot-page.html", "knockout-mapping"], function (ko, homeTemplate, mapping) {
+import ko from 'knockout';
+import homeTemplate from './circular-plot-page.html';
+import mapping from 'knockout-mapping';
 
-    function CircularPlotPage(route) {
-        var self = this;
-        this.configJSON = ko.pureComputed({
-            read: function () {
-                return mapping.toJSON({config: self.plotConfig, data: self.plotData});
-            },
-            write: function (value) {
-                var json = JSON.parse(value);
-                for (propertyName in self.plotConfig) {
-                    if (self.plotConfig.hasOwnProperty(propertyName)) {
-                        self.plotConfig[propertyName](json.config[propertyName]);
-                    }
+function CircularPlotPage(route) {
+    var self = this;
+    this.configJSON = ko.pureComputed({
+        read: function () {
+            return mapping.toJSON({config: self.plotConfig, data: self.plotData});
+        },
+        write: function (value) {
+            var json = JSON.parse(value);
+            for (propertyName in self.plotConfig) {
+                if (self.plotConfig.hasOwnProperty(propertyName)) {
+                    self.plotConfig[propertyName](json.config[propertyName]);
                 }
-                self.dataAsString(json.data.join(", "));
-            },
-            owner: self
-        });
-        this.colorPalette = [
-            "#ffffff", "#000000", "#d6ecff", "#4e5b6f", "#7fd13b", "#ea157a", "#feb80a", "#00addc", "#738ac8", "#1ab39f",
-            "#f2f2f2", "#7f7f7f", "#a7d6ff", "#d9dde4", "#e5f5d7", "#fad0e4", "#fef0cd", "#c5f2ff", "#e2e7f4", "#c9f7f1",
-            "#d8d8d8", "#595959", "#60b5ff", "#b3bcca", "#cbecb0", "#f6a1c9", "#fee29c", "#8be6ff", "#c7d0e9", "#94efe3",
-            "#bfbfbf", "#3f3f3f", "#007dea", "#8d9baf", "#b2e389", "#f272af", "#fed46b", "#51d9ff", "#aab8de", "#5fe7d5",
-            "#a5a5a5", "#262626", "#003e75", "#3a4453", "#5ea226", "#af0f5b", "#c58c00", "#0081a5", "#425ea9", "#138677",
-            "#7f7f7f", "#0c0c0c", "#00192e", "#272d37", "#3f6c19", "#750a3d", "#835d00", "#00566e", "#2c3f71", "#0c594f"
-        ];
-        this.message = ko.observable('Welcome to webStats!');
-        this.dataAsString = ko.observable(null);
-
-        this.plotData = ko.computed(function () {
-            console.log(self.dataAsString());
-            if (self.dataAsString()) {
-                return self.dataAsString().split(",").map(parseFloat).filter(function (value) {
-                    return !Number.isNaN(value);
-                });
             }
-            return [];
-        });
-        this.plotConfig = {
-            lengthOfDay: ko.observable(24).extend({numeric: {precision: 2, defaultValue: 24}}), //
-            mainAxisRadius: ko.observable(200).extend({numeric: {}}), //
-            mainAxisWidth: ko.observable(2).extend({numeric: {}}), //
-            mainAxisColour: ko.observable('#333333'), //
-            axisTickWidth: ko.observable(1).extend({numeric: {}}),//
-            axisTickColour: ko.observable('#333333'),//
-            axisTickLength: ko.observable(6).extend({numeric: {}}),//
-            binTickWidth: ko.observable(1).extend({numeric: {}}),//
-            binTickColour: ko.observable('#777777'),//
-            binTickSize: ko.observable(4).extend({numeric: {}}),//
-            binSize: ko.observable(0.25).extend({numeric: {precision: 2, defaultValue: 0.25}}),//
-            blobColour: ko.observable('#d9edf7'),//
-            modalBlobColour: ko.observable('#fcf8e3'),//
-            meanBlobColour: ko.observable('#dff0d8'),//
-            meanBlobSize: ko.observable(20).extend({numeric: {}}),
-            magnitudeBlobColour: ko.observable('#f2dede'),//
-            magnitudeBlobSize: ko.observable(15).extend({numeric: {}}),
-            magnitudeLineWidth: ko.observable(2).extend({numeric: {}}),
-            blobLineWidth: ko.observable(2).extend({numeric: {}}),
-            blobSize: ko.observable(10).extend({numeric: {}}),
-            blobSep: ko.observable(5).extend({numeric: {}}),
-            canvasSize: ko.observable(500).extend({numeric: {}})
-        };
-        this.plotStats = mapping.fromJS({
-            circularMean: null,
-            vectorMagnitude: null,
-            dataPointCount: null
-        });
-    }
+            self.dataAsString(json.data.join(", "));
+        },
+        owner: self
+    });
+    this.colorPalette = [
+        "#ffffff", "#000000", "#d6ecff", "#4e5b6f", "#7fd13b", "#ea157a", "#feb80a", "#00addc", "#738ac8", "#1ab39f",
+        "#f2f2f2", "#7f7f7f", "#a7d6ff", "#d9dde4", "#e5f5d7", "#fad0e4", "#fef0cd", "#c5f2ff", "#e2e7f4", "#c9f7f1",
+        "#d8d8d8", "#595959", "#60b5ff", "#b3bcca", "#cbecb0", "#f6a1c9", "#fee29c", "#8be6ff", "#c7d0e9", "#94efe3",
+        "#bfbfbf", "#3f3f3f", "#007dea", "#8d9baf", "#b2e389", "#f272af", "#fed46b", "#51d9ff", "#aab8de", "#5fe7d5",
+        "#a5a5a5", "#262626", "#003e75", "#3a4453", "#5ea226", "#af0f5b", "#c58c00", "#0081a5", "#425ea9", "#138677",
+        "#7f7f7f", "#0c0c0c", "#00192e", "#272d37", "#3f6c19", "#750a3d", "#835d00", "#00566e", "#2c3f71", "#0c594f"
+    ];
+    this.message = ko.observable('Welcome to webStats!');
+    this.dataAsString = ko.observable(null);
 
-    CircularPlotPage.prototype.doSomething = function () {
-        this.plotConfig.lengthOfDay(20);
-        this.message('You invoked doSomething() on the viewmodel.');
+    this.plotData = ko.computed(function () {
+        console.log(self.dataAsString());
+        if (self.dataAsString()) {
+            return self.dataAsString().split(",").map(parseFloat).filter(function (value) {
+                return !Number.isNaN(value);
+            });
+        }
+        return [];
+    });
+    this.plotConfig = {
+        lengthOfDay: ko.observable(24).extend({numeric: {precision: 2, defaultValue: 24}}), //
+        mainAxisRadius: ko.observable(200).extend({numeric: {}}), //
+        mainAxisWidth: ko.observable(2).extend({numeric: {}}), //
+        mainAxisColour: ko.observable('#333333'), //
+        axisTickWidth: ko.observable(1).extend({numeric: {}}),//
+        axisTickColour: ko.observable('#333333'),//
+        axisTickLength: ko.observable(6).extend({numeric: {}}),//
+        binTickWidth: ko.observable(1).extend({numeric: {}}),//
+        binTickColour: ko.observable('#777777'),//
+        binTickSize: ko.observable(4).extend({numeric: {}}),//
+        binSize: ko.observable(0.25).extend({numeric: {precision: 2, defaultValue: 0.25}}),//
+        blobColour: ko.observable('#d9edf7'),//
+        modalBlobColour: ko.observable('#fcf8e3'),//
+        meanBlobColour: ko.observable('#dff0d8'),//
+        meanBlobSize: ko.observable(20).extend({numeric: {}}),
+        magnitudeBlobColour: ko.observable('#f2dede'),//
+        magnitudeBlobSize: ko.observable(15).extend({numeric: {}}),
+        magnitudeLineWidth: ko.observable(2).extend({numeric: {}}),
+        blobLineWidth: ko.observable(2).extend({numeric: {}}),
+        blobSize: ko.observable(10).extend({numeric: {}}),
+        blobSep: ko.observable(5).extend({numeric: {}}),
+        canvasSize: ko.observable(500).extend({numeric: {}})
     };
+    this.plotStats = mapping.fromJS({
+        circularMean: null,
+        vectorMagnitude: null,
+        dataPointCount: null
+    });
+}
 
-    return {viewModel: CircularPlotPage, template: homeTemplate};
+CircularPlotPage.prototype.doSomething = function () {
+    this.plotConfig.lengthOfDay(20);
+    this.message('You invoked doSomething() on the viewmodel.');
+};
 
-});
+export default {viewModel: CircularPlotPage, template: homeTemplate};
--- src/components/circular-plot/circular-plot.js
+++ src/components/circular-plot/circular-plot.js
@@ -1,245 +1,247 @@
 /**
  * Created by tom on 28/06/16.
  */
-define(['knockout', './circular-plot.html', 'jquery', 'colour'], function (ko, templateMarkup, jquery, tinycolour) {
 
-    /**
-     * New circular plot
-     * @param element - the DOM element within which this component is contained.
-     * @param params - must comtain at least 'config' and 'data' objects.
-     * @constructor
-     */
-    function CircularPlot(element, params) {
-        var self = this;
-        self.canvas = jquery(element).find("#chart-canvas").get(0);
-        var listener = function () {
-            updateChart(self.canvas, ko.toJS(params.config), ko.toJS(params.data), params.stats);
-        };
-        self.subscriptions = [];
-        self.subscriptions.push(ko.computed(function () {
-            return ko.toJSON(params.config)
-        }).subscribe(listener));
-        self.subscriptions.push(params.data.subscribe(listener));
-        self.canvasSize = params.config.canvasSize();
-        listener();
+import ko from 'knockout';
+import templateMarkup from './circular-plot.html';
+import jquery from 'jquery';
+import tinycolour from 'colour';
+
+/**
+ * New circular plot
+ * @param element - the DOM element within which this component is contained.
+ * @param params - must comtain at least 'config' and 'data' objects.
+ * @constructor
+ */
+function CircularPlot(element, params) {
+    var self = this;
+    self.canvas = jquery(element).find("#chart-canvas").get(0);
+    var listener = function () {
+        updateChart(self.canvas, ko.toJS(params.config), ko.toJS(params.data), params.stats);
+    };
+    self.subscriptions = [];
+    self.subscriptions.push(ko.computed(function () {
+        return ko.toJSON(params.config)
+    }).subscribe(listener));
+    self.subscriptions.push(params.data.subscribe(listener));
+    self.canvasSize = params.config.canvasSize();
+    listener();
+}
+
+/**
+ * Teardown logic, remove subscriptions to data and config.
+ */
+CircularPlot.prototype.dispose = function () {
+    var self = this;
+    self.subscriptions.forEach(function (subscription) {
+        subscription.dispose();
+    });
+    //console.log("Unsubscribing...");
+};
+
+/**
+ * Update the chart.
+ * @param canvas the Canvas object
+ * @param extraConfig overrides default config values
+ * @param data an array of floating point numbers
+ * @param stats an observable object, with observable floating point values 'circularMean', 'vectorMagnitude' and
+ * 'dataPointCount' which will be written to when the chart updates.
+ */
+function updateChart(canvas, extraConfig, data, stats) {
+
+    var ctx = canvas.getContext("2d");
+
+    var config = jquery.extend({
+        lengthOfDay: 24,
+        mainAxisRadius: 200,
+        mainAxisWidth: 2,
+        mainAxisColour: '#333333',
+        axisTickWidth: 1,
+        axisTickColour: '#333333',
+        axisTickLength: 6,
+        binTickWidth: 1,
+        binTickColour: '#777777',
+        binTickSize: 4,
+        binSize: 0.25,
+        blobColour: '#d9edf7',
+        modalBlobColour: '#fcf8e3',
+        meanBlobColour: '#dff0d8',
+        meanBlobSize: 20,
+        magnitudeBlobColour: '#f2dede',
+        magnitudeBlobSize: 15,
+        magnitudeLineWidth: 2,
+        blobLineWidth: 2,
+        blobSize: 10,
+        blobSep: 5,
+        canvasSize: 500
+    }, extraConfig);
+
+    //console.log(config, data);
+
+    var hourToRadians = function (hour) {
+        return Math.PI * 2 * hour / config.lengthOfDay;
+    };
+
+    function radiansToHours(radians) {
+        return radians / (Math.PI * 2) * config.lengthOfDay;
     }
 
-    /**
-     * Teardown logic, remove subscriptions to data and config.
-     */
-    CircularPlot.prototype.dispose = function () {
-        var self = this;
-        self.subscriptions.forEach(function (subscription) {
-            subscription.dispose();
-        });
-        //console.log("Unsubscribing...");
+    var polarToCart = function (hour, offset) {
+        var radians = hourToRadians(hour) - Math.PI / 2;
+        var radius = config.mainAxisRadius + offset;
+        var sin = Math.sin(radians);
+        var cos = Math.cos(radians);
+        return {
+            x: radius * cos,
+            y: radius * sin
+        }
+    };
+
+    var circularMean = function (sumOfSin, sumOfCos) {
+        var mean = Math.atan(sumOfSin / sumOfCos);
+        if (sumOfCos < 0) {
+            mean += Math.PI;
+        } else if (sumOfSin < 0 && sumOfCos > 0) {
+            mean += Math.PI * 2;
+        }
+        return mean;
     };
 
     /**
-     * Update the chart.
-     * @param canvas the Canvas object
-     * @param extraConfig overrides default config values
-     * @param data an array of floating point numbers
-     * @param stats an observable object, with observable floating point values 'circularMean', 'vectorMagnitude' and
-     * 'dataPointCount' which will be written to when the chart updates.
+     * Draw a blob
+     * @param coordinates {x, y} coordinates for the blob centre
+     * @param radius radius in pixels
+     * @param colour colour to use
      */
-    function updateChart(canvas, extraConfig, data, stats) {
-
-        var ctx = canvas.getContext("2d");
-
-        var config = jquery.extend({
-            lengthOfDay: 24,
-            mainAxisRadius: 200,
-            mainAxisWidth: 2,
-            mainAxisColour: '#333333',
-            axisTickWidth: 1,
-            axisTickColour: '#333333',
-            axisTickLength: 6,
-            binTickWidth: 1,
-            binTickColour: '#777777',
-            binTickSize: 4,
-            binSize: 0.25,
-            blobColour: '#d9edf7',
-            modalBlobColour: '#fcf8e3',
-            meanBlobColour: '#dff0d8',
-            meanBlobSize: 20,
-            magnitudeBlobColour: '#f2dede',
-            magnitudeBlobSize: 15,
-            magnitudeLineWidth: 2,
-            blobLineWidth: 2,
-            blobSize: 10,
-            blobSep: 5,
-            canvasSize: 500
-        }, extraConfig);
-
-        //console.log(config, data);
-
-        var hourToRadians = function (hour) {
-            return Math.PI * 2 * hour / config.lengthOfDay;
-        };
-
-        function radiansToHours(radians) {
-            return radians / (Math.PI * 2) * config.lengthOfDay;
+    var drawBlob = function (coordinates, radius, colour) {
+        if (!colour) {
+            colour = config.blobColour;
         }
-
-        var polarToCart = function (hour, offset) {
-            var radians = hourToRadians(hour) - Math.PI / 2;
-            var radius = config.mainAxisRadius + offset;
-            var sin = Math.sin(radians);
-            var cos = Math.cos(radians);
-            return {
-                x: radius * cos,
-                y: radius * sin
-            }
-        };
-
-        var circularMean = function (sumOfSin, sumOfCos) {
-            var mean = Math.atan(sumOfSin / sumOfCos);
-            if (sumOfCos < 0) {
-                mean += Math.PI;
-            } else if (sumOfSin < 0 && sumOfCos > 0) {
-                mean += Math.PI * 2;
-            }
-            return mean;
-        };
-
-        /**
-         * Draw a blob
-         * @param coordinates {x, y} coordinates for the blob centre
-         * @param radius radius in pixels
-         * @param colour colour to use
-         */
-        var drawBlob = function (coordinates, radius, colour) {
-            if (!colour) {
-                colour = config.blobColour;
-            }
-            ctx.save();
-            ctx.strokeStyle = tinycolour(colour).darken(50).toHexString();
-            ctx.fillStyle = tinycolour(colour).toHexString();
-            ctx.lineWidth = config.blobLineWidth;
-            ctx.beginPath();
-            ctx.arc(coordinates.x, coordinates.y, radius, 0, Math.PI * 2);
-            ctx.fill();
-            ctx.stroke();
-            ctx.closePath();
-            ctx.restore();
-        };
-
-        /**
-         * Draw the axis
-         */
-        var initCanvas = function () {
-            canvas.width = config.canvasSize;
-            canvas.height = config.canvasSize;
-            ctx.translate(config.canvasSize / 2, config.canvasSize / 2);
-            ctx.save();
-            ctx.strokeStyle = config.mainAxisColour;
-            ctx.lineWidth = config.mainAxisWidth;
-            ctx.beginPath();
-            ctx.arc(0, 0, config.mainAxisRadius, 0, Math.PI * 2);
-            ctx.stroke();
-            ctx.closePath();
-            ctx.strokeStyle = config.axisTickColour;
-            ctx.lineWidth = config.axisTickWidth;
-            for (var hour = 0; hour < config.lengthOfDay; hour++) {
-                var from = polarToCart(hour, config.mainAxisWidth / 2);
-                var to = polarToCart(hour, config.mainAxisWidth / 2 + config.axisTickLength);
-                ctx.beginPath();
-                ctx.moveTo(from.x, from.y);
-                ctx.lineTo(to.x, to.y);
-                ctx.stroke();
-                ctx.closePath();
-            }
-            ctx.strokeStyle = config.binTickColour;
-            ctx.lineWidth = config.binTickWidth;
-            for (var binHour = 0; binHour < config.lengthOfDay; binHour += config.binSize) {
-                var tickFrom = polarToCart(binHour, -config.mainAxisWidth / 2);
-                var tickTo = polarToCart(binHour, -(config.binTickSize + config.mainAxisWidth / 2));
-                ctx.beginPath();
-                ctx.moveTo(tickFrom.x, tickFrom.y);
-                ctx.lineTo(tickTo.x, tickTo.y);
-                ctx.stroke();
-                ctx.closePath();
-            }
-            ctx.restore();
-        };
-
-        var hist = new Array(config.lengthOfDay / config.binSize).fill(0);
-        var validDataPoints = 0;
-        var sumOfSin = 0.0;
-        var sumOfCos = 0.0;
-        var maxCount = 0;
-        var allCoords = [];
-
-        data.forEach(function (value) {
-            if (!Number.isNaN(value)) {
-                allCoords.push(polarToCart(value % config.lengthOfDay, 0));
-                validDataPoints++;
-                var bin = Math.floor((value / config.binSize) % (config.lengthOfDay / config.binSize));
-                hist[bin]++;
-                maxCount = Math.max(maxCount, hist[bin]);
-                sumOfSin += Math.sin(hourToRadians(value));
-                sumOfCos += Math.cos(hourToRadians(value));
-            }
-        });
-
-        initCanvas();
-
-        //console.log(hist);
-
-        hist.forEach(function (binCount, binIndex) {
-            var colour = config.blobColour;
-            if (binCount == maxCount) {
-                colour = config.modalBlobColour;
-            }
-            var binHour = (binIndex + 0.5) * config.binSize;
-            for (var count = 0; count < binCount; count++) {
-                drawBlob(polarToCart(binHour, -(count * (config.blobSize + config.blobSep)
-                + config.blobSep + config.blobSize / 2)), config.blobSize / 2, colour);
-            }
-        });
-
-        // Show stats if we have some data points
-        if (validDataPoints > 0) {
-            var meanHour = radiansToHours(circularMean(sumOfSin, sumOfCos));
-            var magnitude = Math.sqrt(Math.pow(sumOfSin / validDataPoints, 2) + Math.pow(sumOfCos / validDataPoints, 2));
-            //console.log("Magnitude is ", magnitude);
-            var meanPoint = polarToCart(meanHour, 0);
-            var magnitudePoint = polarToCart(meanHour, -(1 - magnitude) * config.mainAxisRadius);
-            ctx.save();
-            ctx.strokeStyle = tinycolour(config.magnitudeBlobColour).darken(50).toHexString();
-            ctx.lineWidth = config.magnitudeLineWidth;
-            ctx.beginPath();
-            ctx.moveTo(0, 0);
-            ctx.lineTo(magnitudePoint.x, magnitudePoint.y);
-            ctx.stroke();
-            ctx.closePath();
-            ctx.strokeStyle = config.mainAxisColour;
-            ctx.beginPath();
-            ctx.moveTo(magnitudePoint.x, magnitudePoint.y);
-            ctx.lineTo(meanPoint.x, meanPoint.y);
-            ctx.stroke();
-            ctx.closePath();
-            ctx.restore();
-            drawBlob(meanPoint, config.meanBlobSize / 2, config.meanBlobColour);
-            drawBlob(magnitudePoint, config.magnitudeBlobSize / 2, config.magnitudeBlobColour);
-            stats.circularMean(meanHour);
-            stats.vectorMagnitude(magnitude);
-            stats.dataPointCount(validDataPoints);
-        }
-
-
-    }
-
-    return {
-        // Pass in a createView function so our constructor has access to the DOM element, this is
-        // then used to find the Canvas element so we can draw stuff into it.
-        viewModel: {
-            createViewModel: function (params, componentInfo) {
-                return new CircularPlot(componentInfo.element, params);
-            }
-        },
-        template: templateMarkup
+        ctx.save();
+        ctx.strokeStyle = tinycolour(colour).darken(50).toHexString();
+        ctx.fillStyle = tinycolour(colour).toHexString();
+        ctx.lineWidth = config.blobLineWidth;
+        ctx.beginPath();
+        ctx.arc(coordinates.x, coordinates.y, radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+        ctx.restore();
     };
 
-});
+    /**
+     * Draw the axis
+     */
+    var initCanvas = function () {
+        canvas.width = config.canvasSize;
+        canvas.height = config.canvasSize;
+        ctx.translate(config.canvasSize / 2, config.canvasSize / 2);
+        ctx.save();
+        ctx.strokeStyle = config.mainAxisColour;
+        ctx.lineWidth = config.mainAxisWidth;
+        ctx.beginPath();
+        ctx.arc(0, 0, config.mainAxisRadius, 0, Math.PI * 2);
+        ctx.stroke();
+        ctx.closePath();
+        ctx.strokeStyle = config.axisTickColour;
+        ctx.lineWidth = config.axisTickWidth;
+        for (var hour = 0; hour < config.lengthOfDay; hour++) {
+            var from = polarToCart(hour, config.mainAxisWidth / 2);
+            var to = polarToCart(hour, config.mainAxisWidth / 2 + config.axisTickLength);
+            ctx.beginPath();
+            ctx.moveTo(from.x, from.y);
+            ctx.lineTo(to.x, to.y);
+            ctx.stroke();
+            ctx.closePath();
+        }
+        ctx.strokeStyle = config.binTickColour;
+        ctx.lineWidth = config.binTickWidth;
+        for (var binHour = 0; binHour < config.lengthOfDay; binHour += config.binSize) {
+            var tickFrom = polarToCart(binHour, -config.mainAxisWidth / 2);
+            var tickTo = polarToCart(binHour, -(config.binTickSize + config.mainAxisWidth / 2));
+            ctx.beginPath();
+            ctx.moveTo(tickFrom.x, tickFrom.y);
+            ctx.lineTo(tickTo.x, tickTo.y);
+            ctx.stroke();
+            ctx.closePath();
+        }
+        ctx.restore();
+    };
+
+    var hist = new Array(config.lengthOfDay / config.binSize).fill(0);
+    var validDataPoints = 0;
+    var sumOfSin = 0.0;
+    var sumOfCos = 0.0;
+    var maxCount = 0;
+    var allCoords = [];
+
+    data.forEach(function (value) {
+        if (!Number.isNaN(value)) {
+            allCoords.push(polarToCart(value % config.lengthOfDay, 0));
+            validDataPoints++;
+            var bin = Math.floor((value / config.binSize) % (config.lengthOfDay / config.binSize));
+            hist[bin]++;
+            maxCount = Math.max(maxCount, hist[bin]);
+            sumOfSin += Math.sin(hourToRadians(value));
+            sumOfCos += Math.cos(hourToRadians(value));
+        }
+    });
+
+    initCanvas();
+
+    //console.log(hist);
+
+    hist.forEach(function (binCount, binIndex) {
+        var colour = config.blobColour;
+        if (binCount == maxCount) {
+            colour = config.modalBlobColour;
+        }
+        var binHour = (binIndex + 0.5) * config.binSize;
+        for (var count = 0; count < binCount; count++) {
+            drawBlob(polarToCart(binHour, -(count * (config.blobSize + config.blobSep)
+                                            + config.blobSep + config.blobSize / 2)), config.blobSize / 2, colour);
+        }
+    });
+
+    // Show stats if we have some data points
+    if (validDataPoints > 0) {
+        var meanHour = radiansToHours(circularMean(sumOfSin, sumOfCos));
+        var magnitude = Math.sqrt(Math.pow(sumOfSin / validDataPoints, 2) + Math.pow(sumOfCos / validDataPoints, 2));
+        //console.log("Magnitude is ", magnitude);
+        var meanPoint = polarToCart(meanHour, 0);
+        var magnitudePoint = polarToCart(meanHour, -(1 - magnitude) * config.mainAxisRadius);
+        ctx.save();
+        ctx.strokeStyle = tinycolour(config.magnitudeBlobColour).darken(50).toHexString();
+        ctx.lineWidth = config.magnitudeLineWidth;
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.lineTo(magnitudePoint.x, magnitudePoint.y);
+        ctx.stroke();
+        ctx.closePath();
+        ctx.strokeStyle = config.mainAxisColour;
+        ctx.beginPath();
+        ctx.moveTo(magnitudePoint.x, magnitudePoint.y);
+        ctx.lineTo(meanPoint.x, meanPoint.y);
+        ctx.stroke();
+        ctx.closePath();
+        ctx.restore();
+        drawBlob(meanPoint, config.meanBlobSize / 2, config.meanBlobColour);
+        drawBlob(magnitudePoint, config.magnitudeBlobSize / 2, config.magnitudeBlobColour);
+        stats.circularMean(meanHour);
+        stats.vectorMagnitude(magnitude);
+        stats.dataPointCount(validDataPoints);
+    }
+
+
+}
+
+export default {
+    // Pass in a createView function so our constructor has access to the DOM element, this is
+    // then used to find the Canvas element so we can draw stuff into it.
+    viewModel: {
+        createViewModel: function (params, componentInfo) {
+            return new CircularPlot(componentInfo.element, params);
+        }
+    },
+    template: templateMarkup
+};
